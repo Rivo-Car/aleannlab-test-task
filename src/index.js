1d8ce0bca52358e473060b402c1dc184362b2b23
Rivo-Car <eduard.billionaire@gmail.com>
2022-11-18 15:17:52 +0200

    added ejs templates, added name list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-const http = require('http');
-const { isHelloworld, processHelloWorld } = require('./handlers/HelloWorld');
-const { isStatic, processStatic } = require('./handlers/StaticHandler');
-const { respondWith404 } = require('./handlers/404');
+const express = require('express');
+const path = require('path');
 
-const server = http.createServer(
-  (req, res) => {
-    const currentUrl = req.url;
-    if (isHelloworld(currentUrl)) {
-      processHelloWorld(req, res);
-      return;
-    } if (isStatic(currentUrl)) {
-      processStatic(req, res);
-      return;
-    }
-    respondWith404(res);
-  },
-);
-server.listen(3000);
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static(
+  path.join(process.cwd(), './static'),
+  { redirect: false },
+));
+
+app.get('/names', (req, res) => {
+  res.render('names', {
+    names: [
+      { id: 1, name: 'Name 1', rank: 10 },
+      { id: 2, name: 'Name 2', rank: 20 },
+    ],
+  });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is listening: http://localhost:3000');
+});
